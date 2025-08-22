bf72990017f8f7b075444fe719c1ef1167b22d44
Bebe Kote <lifetroll6@gmail.com>
2025-08-22 23:16:27 +0300

    created react project and added simple UI for resources. Need more changes

---
+++ discord404/src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>404-Resource-Bunker</h1>
+      <h3>made by <a href="https://discord.com/invite/9yNUd34x">404 Discord Group</a></h3>
+      <ul>
+        <li>
+          <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+            React Documentation
+          </a>
+        </li>
+        <li>
+          <a href="https://developer.mozilla.org/" target="_blank" rel="noopener noreferrer">
+            MDN Web Docs
+          </a>
+        </li>
+        <li>
+          <a href="https://laravel.com/docs" target="_blank" rel="noopener noreferrer">
+            Laravel Docs
+          </a>
+        </li>
+        <li>
+          <a href="https://roadmap.sh" target="_blank" rel="noopener noreferrer">
+            Developer Roadmaps
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
